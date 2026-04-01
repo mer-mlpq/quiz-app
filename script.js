ec5c534d97ffd7fc550b111dcda7b4ff63529797
Merwan Ibrahim <merwanibrahim@Merwans-MacBook-Air.local>
2026-03-31 17:11:05 -0700

    I finalized the JS and fixed the status page

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const questionNumEl = document.querySelector(".question-num");
 const subjectEl = document.querySelector(".subject");
 const questionEl = document.querySelector(".question");
 const choiceEls = document.querySelectorAll(".choice-item");
+const statusEls = document.querySelectorAll(".status-item");
 const nextBtn = document.querySelector(".next-btn");
 const skipBtn = document.querySelector(".skip-btn");
 
 let currentQuestion = 0;
 let score = 0;
+let selectedAnswer = null;
 
 const questions = [
   {
@@ -44,6 +46,54 @@ function loadQuestion() {
     choiceEl.textContent = q.choices[index];
   });
   nextBtn.disabled = true;
+  selectedAnswer = null;
+}
+
+choiceEls.forEach((choiceBtn, index) => {
+  choiceBtn.addEventListener("click", () => {
+    selectedAnswer = index;
+    choiceEls.forEach((b) => b.classList.remove("selected"));
+
+    choiceBtn.classList.add("selected");
+    nextBtn.disabled = false;
+  });
+});
+
+nextBtn.addEventListener("click", () => {
+  checkAnswer();
+  goToNextQuestion();
+});
+
+skipBtn.addEventListener("click", () => {
+  checkAnswer();
+  goToNextQuestion();
+});
+
+function checkAnswer() {
+  const correctAnswerIndex = questions[currentQuestion].correct;
+  if (selectedAnswer === correctAnswerIndex) {
+    statusEls[currentQuestion].classList.add("correct");
+    score++;
+  } else {
+    statusEls[currentQuestion].classList.add("wrong");
+  }
+}
+
+function goToNextQuestion() {
+  currentQuestion++;
+  if (questions.length > currentQuestion) loadQuestion();
+  else {
+    showFinalScore();
+  }
+}
+
+function showFinalScore() {
+  questionEl.textContent = `You have scored ${score} out of ${questions.length}`;
+  subjectEl.textContent = "";
+  questionNumEl.textContent = "";
+  choiceEls.forEach((btn) => (btn.style.display = "none"));
+  nextBtn.style.display = "none";
+  skipBtn.style.display = "none";
 }
 
 loadQuestion();
